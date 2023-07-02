023b5e875289e7ffd8f704814915c9c75eb2b1c0
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-02 17:55:33 -0500

    Feat: Add .eslintrc for semistandard style

---
+++ closures_and_scopes/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'standard',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-extra-semi': 'error'
+  }
+};
