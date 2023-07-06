53aa025fae8ea7e529aa74567528180213cff2cf
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-06 16:38:00 -0500

    Move common node files to the repo root

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    semi: [
+      'error',
+      'always'
+    ]
+  }
+};
