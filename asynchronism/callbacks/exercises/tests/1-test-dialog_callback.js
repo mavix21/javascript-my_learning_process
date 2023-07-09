d6035d6f8632893d25c45b04ed4e96177b1584d7
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-08 19:02:02 -0500

    Add test file for Dialog class

---
+++ asynchronism/callbacks/exercises/tests/1-test-dialog_callback.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const Dialog = require('../1-dialog_callback.js');
+const { assert } = require('chai');
+
+describe('Dialog', () => {
+  it('should allow registering of multiple callbacks', () => {
+    const dialog = new Dialog();
+    let calledA = 0;
+    let calledB = 0;
+    let calledC = 0;
+    dialog.onClose(() => {
+      calledA++;
+    });
+
+    dialog.onClose(() => {
+      calledB++;
+    });
+
+    dialog.onClose(() => {
+      calledC++;
+    });
+
+    dialog.close();
+    assert.equal(calledA, 1, 'First Callback was not called once after close');
+    assert.equal(calledB, 1, 'Second Callback was not called once after close');
+    assert.equal(calledC, 1, 'Third Callback was not called once after close');
+  });
+});
