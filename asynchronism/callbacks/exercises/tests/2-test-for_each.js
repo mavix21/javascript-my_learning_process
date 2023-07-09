593bd60ce488b0ff93d46e00fd0d709d47b9c22f
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-08 19:22:17 -0500

    Add tests for exercise 2

---
+++ asynchronism/callbacks/exercises/tests/2-test-for_each.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const forEach = require('../2-for_each.js');
+const { assert } = require('chai');
+
+describe('forEach', () => {
+  it('should loop over a function for every element', () => {
+    let sum = 0;
+    let iterations = 0;
+    forEach([1, 2, 3, 4], (n, i) => {
+      assert.equal(i, iterations, 'index should be supplied for each iteration');
+      iterations++;
+      sum += n;
+    });
+    assert.equal(iterations, 4, 'Should be a total of four iterations for four elements');
+    assert.equal(sum, 10, 'Should correctly sum four elements');
+  });
+});
