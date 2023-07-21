67b0ca5b2cac9307aa95e347cfb9ecb8ee3433a0
mavix21 <marcevizcarra2109@hotmail.com>
2023-07-21 13:06:15 -0500

    Feat: Pact allows to chain .then callbacks

---
+++ asynchronism/pact/src/6-chaining-callbacks.js
@@ -0,0 +1,49 @@
+const STATUS = {
+  PENDING: 0,
+  RESOLVED: 1,
+  REJECTED: 2
+};
+
+class Pact {
+  constructor (executor) {
+    this.thenFns = [];
+    this.catchFns = [];
+    this.status = STATUS.PENDING;
+
+    this.resolve = (value) => {
+      this.resolvedValue = value;
+      this.status = STATUS.RESOLVED;
+      this.thenFns.forEach(fn => fn(value));
+    };
+
+    this.reject = (err) => {
+      this.rejectedValue = err;
+      this.status = STATUS.REJECTED;
+      this.catchFns.forEach(fn => fn(err));
+    };
+
+    executor(this.resolve, this.reject);
+  }
+
+  then (callback) {
+    if (this.status === STATUS.PENDING) {
+      return new Pact((resolve, reject) => {
+        this.thenFns.push((val) => {
+          resolve(callback(val));
+        });
+      });
+    } else if (this.status === STATUS.RESOLVED) {
+      callback(this.resolvedValue);
+    }
+  }
+
+  catch (callback) {
+    if (this.status === STATUS.PENDING) {
+      this.catchFns.push(callback);
+    } else if (this.status === STATUS.REJECTED) {
+      callback(this.rejectedValue);
+    }
+  }
+}
+
+module.exports = Pact;
